6c98ceed24b52fccfdda48e495245fff6aa4e05c
Ran Ram <p0038974@ness-tech.co.il>
2026-02-06 19:45:07 +0200

    Fix: Add .js extensions to app.ts imports

--- packages/server/src/app.ts
+++ packages/server/src/app.ts
@@ -3,24 +3,24 @@ import helmet from 'helmet';
 import cors from 'cors';
 import morgan from 'morgan';
 
-import { corsOptions } from './config/cors';
-import { errorHandler } from './middleware/error-handler.middleware';
-import { defaultLimiter } from './middleware/rate-limit.middleware';
+import { corsOptions } from './config/cors.js';
+import { errorHandler } from './middleware/error-handler.middleware.js';
+import { defaultLimiter } from './middleware/rate-limit.middleware.js';
 
 // Route imports
-import authRoutes from './modules/auth/auth.routes';
-import projectRoutes from './modules/projects/projects.routes';
-import widgetRoutes from './modules/widgets/widgets.routes';
-import statusRoutes from './modules/statuses/statuses.routes';
-import taskRoutes from './modules/tasks/tasks.routes';
-import permissionRoutes from './modules/permissions/permissions.routes';
-import fileRoutes from './modules/files/files.routes';
-import templateRoutes from './modules/templates/templates.routes';
-import userRoutes from './modules/users/users.routes';
-import adminRoutes from './modules/admin/admin.routes';
-import labelRoutes from './modules/labels/labels.routes';
-import commentRoutes from './modules/comments/comments.routes';
-import activityRoutes from './modules/activity/activity.routes';
+import authRoutes from './modules/auth/auth.routes.js';
+import projectRoutes from './modules/projects/projects.routes.js';
+import widgetRoutes from './modules/widgets/widgets.routes.js';
+import statusRoutes from './modules/statuses/statuses.routes.js';
+import taskRoutes from './modules/tasks/tasks.routes.js';
+import permissionRoutes from './modules/permissions/permissions.routes.js';
+import fileRoutes from './modules/files/files.routes.js';
+import templateRoutes from './modules/templates/templates.routes.js';
+import userRoutes from './modules/users/users.routes.js';
+import adminRoutes from './modules/admin/admin.routes.js';
+import labelRoutes from './modules/labels/labels.routes.js';
+import commentRoutes from './modules/comments/comments.routes.js';
+import activityRoutes from './modules/activity/activity.routes.js';
 
 const createApp = (): express.Application => {
   const app = express();
